f68105dc2043bb0433693ed42e0483787729e713
milushev91 <mmilushev91@gmail.com>
2025-12-10 17:15:51 +0200

    Update variables_and_data_types.py

--- Lectures/variables_and_data_types.py
+++ Lectures/variables_and_data_types.py
@@ -1,9 +1,14 @@
+#4. Variables and  Data Types
 age = 24
-print(age)
-
-print(type(greeting))
-print(type(age))
+print(age) # 24
+# type function returns the type of the variable
+print(type(greeting)) # <class 'str'>
+print(type(age)) # <class 'int'>
 
 age = "2 years"
-print(age)
-print(type(age))
+print(age) # 2 years
+print(type(age)) # <class 'str'>
+
+#python is a stongly typed language
+age_in_words = "2 years"
+print(name + " is " + age + " years old") # TypeError: can only concatenate str (not "int") to str
