079fbd93b5c1b062e35659162867611f09852c1c
milushev91 <mmilushev91@gmail.com>
2025-12-11 14:29:44 +0200

    Create operator_precedence.py

---
+++ Lectures/operator_precedence.py
@@ -0,0 +1,16 @@
+print()
+#PEMDAS - Parentheses, Exponents, Multiplication and Division (from left to right), Addition and Subtraction (from left to right)
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print((((a + b) /3) - 4) * 12)
+print(((a + b) /3 - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+
+print(e * 12)
+
+print()
+
+print(a / (b * a) /b)
