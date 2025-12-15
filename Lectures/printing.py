16c662c05d032c75846123bd23127a823303d8a8
milushev91 <mmilushev91@gmail.com>
2025-12-15 17:04:34 +0200

    Update printing.py

--- Lectures/printing.py
+++ Lectures/printing.py
@@ -1,5 +1,6 @@
-print('Hello, World!')
-print(1 + 2)
-print(7 * 6)
-print()
-print("The end", "or is it?", "keep watching to learn more about Python", 3)
+print("Hello, World!") # Hello World
+print(1 + 2) # 3
+print( 7 * 6 ) # 42
+print() # prints a blank line
+print("The End", "or is it?", "keep watching to learn more about Python", 1)
+# The End or is it? keep watching to learn more about Python 1
