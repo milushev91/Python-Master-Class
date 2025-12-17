3affef997030d53b67c547e9a5eba53a98e88e7f
milushev91 <mmilushev91@gmail.com>
2025-12-17 07:46:25 +0200

    Update variables_and_data_types.py

--- Lectures/variables_and_data_types.py
+++ Lectures/variables_and_data_types.py
@@ -1,14 +1,28 @@
-#4. Variables and  Data Types
+greeting = "Hello"
+name = "Marian"
 age = 24
-print(age) # 24
-# type function returns the type of the variable
-print(type(greeting)) # <class 'str'>
-print(type(age)) # <class 'int'>
+
+print(type(greeting))  # <class 'str'>
+print(type(age))       # <class 'int'>
+
+#Data type represent the type of value a variable holds.
+
+#Python is a dynamically typed language, meaning you don't need to declare
+#the data type of a variable explicitly. The interpreter infers the type
+#based on the value assigned to the variable. So its more like the value has
+#the type rather than the variable itself. But it also mean that you can
+#reassign a variable to a value of a different type later in the code.
 
 age = "2 years"
-print(age) # 2 years
-print(type(age)) # <class 'str'>
 
-#python is a stongly typed language
-age_in_words = "2 years"
-print(name + " is " + age + " years old") # TypeError: can only concatenate str (not "int") to str
+print(age)        # 2 years
+print(type(age))  # <class 'str'>
+
+age = 24 
+
+#Python is strongly typed, meaning that it enforces type constraints.
+#You cannot perform operations that are not supported between different
+#data types without explicit conversion. For example, you cannot concatenate
+#a string and an integer directly.
+print(name + " is " + age + " years old.") # This will raise a TypeError
+# TypeError: can only concatenate str (not "int") to str
