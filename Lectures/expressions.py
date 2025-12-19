53397ceac83a93daf6248e3f87ab41ae9927a589
milushev91 <mmilushev91@gmail.com>
2025-12-19 07:42:07 +0200

    Create expressions.py

---
+++ Lectures/expressions.py
@@ -0,0 +1,13 @@
+# Expression in Python is anything that can be evaluated to produce a value.
+# This includes literals, variables, operators, function calls, 
+# calculations, and more.
+
+for i in range(1, 4):
+    print(f"Current number is: {i}") # Outputs: Current number is: 1, 2, 3
+
+i = 1 
+print(i)  # Outputs: 1
+i = 2 
+print(i)  # Outputs: 2
+i = 3 
+print(i)  # Outputs: 3
