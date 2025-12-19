ca7516c29f15c11fb2af14dff76a3a9a2f4643fc
milushev91 <mmilushev91@gmail.com>
2025-12-19 07:31:17 +0200

    Create operators.py

---
+++ Lectures/operators.py
@@ -0,0 +1,20 @@
+a = 12 
+b = 3 
+
+# Addition
+print(a + b)  # Output: 15 
+
+# Subtraction
+print(a - b)  # Output: 9
+
+# Multiplication
+print(a * b)  # Output: 36
+
+# Division
+print(a / b)  # Output: 4.0
+
+# Integer Division: rounds down to the nearest whole number
+print(a // b)  # Output: 4
+
+# Modulus: returns the remainder of the division
+print(a % b)  # Output: 0
