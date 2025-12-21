7e9f9b6524aa4c19468999e8ad0d7899bd728b76
milushev91 <mmilushev91@gmail.com>
2025-12-21 17:23:41 +0200

    Update number_guessing_game.py

--- Control Flows/Lectures/number_guessing_game.py
+++ Control Flows/Lectures/number_guessing_game.py
@@ -1,1 +1,10 @@
+answer = 5 
 
+user_guess = int(input("Guess a number between 1 and 10: "))
+
+if user_guess > answer:
+    print("Too High!")
+elif user_guess < answer:
+    print("Too Low!")
+else:
+    print("Congratulations. You got it right!")
