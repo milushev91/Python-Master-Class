2e9154a85f74300a3028cec1ca18dfbc2115c46d
milushev91 <mmilushev91@gmail.com>
2025-12-28 16:17:00 +0200

    Update number_guessing_game.py

--- Control Flows/Lectures/number_guessing_game.py
+++ Control Flows/Lectures/number_guessing_game.py
@@ -2,9 +2,56 @@ answer = 5
 
 user_guess = int(input("Guess a number between 1 and 10: "))
 
-if user_guess > answer:
-    print("Too High!")
-elif user_guess < answer:
-    print("Too Low!")
-else:
+if user_guess == answer:
     print("Congratulations. You got it right!")
+else:
+
+    if user_guess > answer:
+        print("Too High!")
+    else:
+        print("Too Low!")
+
+    guess = int(input())
+
+    if guess == answer:
+        print("Congratulations. You got it right!")
+    else:
+        print("Sorry, you have not guessed correctly.")
+
+# if user_guess != answer:
+
+#     if user_guess > answer:
+#         print("Too High!")
+#     else:
+#         print("Too Low!")
+
+#     user_guess = int(input("Guess again: "))
+
+#     if user_guess != answer:
+#         print("Sorry, you have not guessed correctly.")
+#     else:
+#         print("Congratulations. You got it right!")
+# else:
+#     print("Congratulations. You got it right!")
+
+# if user_guess > answer:
+#     print("Too High!")
+
+#     user_guess = int(input("Guess again: "))
+
+#     if user_guess == answer:
+#         print("Congratulations. You got it right!")
+#     else:
+#         print("Sorry, you have not guessed correctly.")
+
+# elif user_guess < answer:
+#     print("Too Low!")
+
+#     user_guess = int(input("Guess again: "))
+    
+#     if user_guess == answer:
+#         print("Congratulations. You got it right!")
+#     else:
+#         print("Sorry, you have not guessed correctly.")
+# else:
+#     print("Congratulations. You got it right!")
