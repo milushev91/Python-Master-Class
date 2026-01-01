3af5509b1e1ae937646c27609e3eb11aa21b464f
milushev91 <mmilushev91@gmail.com>
2026-01-01 16:34:22 +0200

    Create challenge.py

---
+++ Control Flows/Lectures/challenge.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18 and age < 31:
+    print("Welcome to our holiday!")
+else:
+    print("You aren't at our target age group")
