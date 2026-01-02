96b4f386db408a6da22fd5d14ddfd6e15bc94de1
milushev91 <mmilushev91@gmail.com>
2026-01-02 19:59:47 +0200

    Create for_loops.py

---
+++ Foor Loops/Lectures/for_loops.py
@@ -0,0 +1,9 @@
+parrot = "Norwegian Blue"
+
+#for loop can be used to iterate over every iterable object
+#a iterable object is an object that can return its elements one at a time
+#for loop begins with the keyword 'for' followed by a variable name that will
+#hold each element of the iterable object during each iteration
+#then the keyword 'in' followed by the iterable object to be looped over
+for character in parrot:
+    print(character)
