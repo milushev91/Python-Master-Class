20155a4ee510784d601d5143b3fff0293fb15fc7
milushev91 <mmilushev91@gmail.com>
2026-01-03 16:49:22 +0200

    Create break_statement.py

---
+++ Foor Loops/Lectures/break_statement.py
@@ -0,0 +1,8 @@
+shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
+
+for item in shopping_list:
+
+    if item == "spam":
+        #break statement stops the loop entirely
+        break
+    print("Buy " + item)
