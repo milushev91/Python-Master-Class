b76561e0c3cb3d0d59b81e5e1f9a9ff6c509cf80
milushev91 <mmilushev91@gmail.com>
2026-01-04 19:06:02 +0200

    Create while_loop.py

---
+++ While Loop/Lecture/while_loop.py
@@ -0,0 +1,12 @@
+print("Foor loop")
+for i in range(10):
+    print("i is now {}".format(i))
+
+print("-" * 10)
+
+print("While loop")
+i = 0 
+
+while i < 10:
+    print("i is now {}".format(i))
+    i += 1
