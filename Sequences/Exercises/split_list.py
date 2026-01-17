30895d214ddab726f092401aa1635da4cd2c885b
milushev91 <mmilushev91@gmail.com>
2026-01-17 15:57:46 +0200

    Create split_list.py

---
+++ Sequences/Exercises/split_list.py
@@ -0,0 +1,32 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+# write your code here
+for item in data:
+    name, item_type = item.split(" - ")
+    if item_type == "Flower":
+        flowers.append(name)
+    else:
+        shrubs.append(name)
