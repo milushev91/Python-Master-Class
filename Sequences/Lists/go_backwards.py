e1746d8e0487d8abf9ad3049c093e544df0257de
milushev91 <mmilushev91@gmail.com>
2026-01-19 16:54:14 +0200

    Update go_backwards.py

--- Sequences/Lists/go_backwards.py
+++ Sequences/Lists/go_backwards.py
@@ -4,6 +4,16 @@ min_value = 100
 max_value = 200 
 
 # Safely removing items by iterating backwards
-for index in range(len(data)-1, -1, -1):
-    if data[index] < min_value or data[index] > max_value:
-        del data[index]
+# for index in range(len(data)-1, -1, -1):
+#     if data[index] < min_value or data[index] > max_value:
+#         del data[index]
+
+# Alternative method using reversed and enumerate
+# its slightly less efficient due to the overhead of enumerate and reversed
+# but often more readable
+top_index = len(data) - 1
+for index, value in enumerate(reversed(data)):
+    if value < min_value or value > max_value:
+        del data[top_index - index]
+print(data)  # Output the modified list
+# Output: [101, 104, 105, 185, 120, 130, 150, 160, 170, 183, 187, 188, 191]
