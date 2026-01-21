d125c1fdd63152a4f21628504865f0dbb7623f03
milushev91 <mmilushev91@gmail.com>
2026-01-21 07:44:43 +0200

    Create print_func.py

---
+++ Sequences/print_func.py
@@ -0,0 +1,8 @@
+name = "Tim"
+age = 10
+
+# *objects parameter is used to print multiple values
+# sep parameter is used to define separator between values
+# default separator is space
+print(age, name, "Python", 2020) # 10 Tim Python 2020
+print(age, name, "Python", 2020, sep=", ") # 10, Tim, Python, 2020
