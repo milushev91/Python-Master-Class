2c592ea04e4ffc6d2c22415c0b90a8165915c1d4
milushev91 <mmilushev91@gmail.com>
2026-01-22 17:20:12 +0200

    Create tuples_intro.py

---
+++ Sequences/tuples/tuples_intro.py
@@ -0,0 +1,6 @@
+# creating a tuple
+# they could be created without () but there cases where () are required
+# in order not to remember them you could simply always
+t = (a, b, c)
+
+print(t) # (a, b, c)
