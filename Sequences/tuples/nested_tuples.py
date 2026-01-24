48ad2f2a8a50491097c74ed26210dd8cbe1177b3
milushev91 <mmilushev91@gmail.com>
2026-01-24 11:08:08 +0200

    Create nested_tuples.py

---
+++ Sequences/tuples/nested_tuples.py
@@ -0,0 +1,11 @@
+albums = [ 
+    ("Welcome to my Nightmare", "Alice Cooper", 1975),
+    ("Bad Company", "Bad Company", 1974),
+    ("Budgie", "Budgie", 1971),
+    ("Imelda May", "Imelda May", 2011),
+    ("Metallica", "Metallica", 1993),
+]
+
+for album, artist, year in albums:
+    print(f"Album: {album}, Artist: {artist}, Year: {year}")
+
