a485dbdc8e59c52cfbcb28c4e700450e5cd514c8
milushev91 <mmilushev91@gmail.com>
2026-01-27 07:32:30 +0200

    Update get_valid_integer.py

--- Functions/get_valid_integer.py
+++ Functions/get_valid_integer.py
@@ -1,8 +1,17 @@
 def get_integer(prompt):
+    """
+    Get integer from Standard Input(stdin).
+
+    The function will continue looping, and prompting the user,
+    until valid `int` is entered.
+    
+    :param prompt: The String that the user will see, when they are prompted
+        to enter a value.
+    :return: The integer that the user enters
+    """
     while not prompt.isnumeric():
         prompt = input("Please enter an integer: ")
     return int(prompt)
-
 
 integer = get_integer(input("Please enter an integer: "))
 print(integer)
