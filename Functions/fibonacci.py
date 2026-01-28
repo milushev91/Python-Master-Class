3e50f280414f517e5f4c89736792735b7a7e17ad
milushev91 <mmilushev91@gmail.com>
2026-01-28 07:29:11 +0200

    Create fibonacci.py

---
+++ Functions/fibonacci.py
@@ -0,0 +1,28 @@
+def fibonacci(n):
+    """
+    Generates a fibonacci sequence until last number > n, when value greater than 1 is given,
+    returns n. Raises ValueError if negative value of n is recieved. 
+    
+    :param n (int): Represents the end point of the sequence with n exsclusive.
+    :return (str): Fibonacci sequence represented as String or n as Integer.
+    """
+
+    if n < 0:
+        raise ValueError("Negative number not allowed!")
+    
+    if 0 <= n <= 1:
+        return n
+    numbers = [0, 1]
+    number = 0
+    index = 1
+
+    while True:
+        number = numbers[index] + numbers[index - 1]
+        if number > n:
+            break
+        numbers.append(number)
+        index += 1
+    
+    return [str(num) for num in numbers]
+
+print(fibonacci(36))
