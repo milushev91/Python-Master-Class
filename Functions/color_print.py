12a92d3d93832bfa76d3120c9f85dcc4ede91752
milushev91 <mmilushev91@gmail.com>
2026-01-29 07:48:51 +0200

    Create color_print.py

---
+++ Functions/color_print.py
@@ -0,0 +1,31 @@
+# Some ANSI escape sequences for colours and effects
+BLACK = '\u001b[30m'
+RED = '\u001b[31m'
+GREEN = '\u001b[32m'
+YELLOW = '\u001b[33m'
+BLUE = '\u001b[34m'
+MAGENTA = '\u001b[35m'
+CYAN = '\u001b[36m'
+WHITE = '\u001b[37m'
+RESET = '\u001b[0m'
+ 
+BOLD = '\u001b[1m'
+UNDERLINE = '\u001b[4m'
+REVERSE = '\u001b[7m'
+
+
+def color_print(text: str, effect: str) -> None:
+    """
+    PRINT text using ANSI sequences to change color, etc.
+    
+    :param text: The text to print
+    :type text: str
+    :param effect: The effect we want. One of the constants defined
+    at the start of this module.
+    :type effect: str
+    :return: None
+    """
+    print(effect, text)
+
+color_print(text="this will be red", effect=RED)
+color_print(text="reset text color", effect=RESET)
