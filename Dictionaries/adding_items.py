89fcb953d383a9515b4280e6b3dc1f650ddfae30
milushev91 <mmilushev91@gmail.com>
2026-02-01 09:24:26 +0200

    Create adding_items.py

---
+++ Dictionaries/adding_items.py
@@ -0,0 +1,14 @@
+vehicles = {
+    "PB7608HB": "Mazda 2",
+    "PB7602HB": "Mazda 3",
+    "PB7604HB": "Mazda 5",
+    "PB7603HB": "Mazda 6",
+    "PB7605HB": "Ford Escort",
+    "PB7607HB": "Ford Fiesta",
+    "PB7606HB": "Ford Focus",
+    "PB7609HB": "Opel Astra",
+}
+
+#adding value to a dictionary
+vehicles["starfighter"] = "Lockheed F-104"
+vehicles["toy"] = "glider"
