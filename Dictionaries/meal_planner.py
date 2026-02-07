8b826a2f72b8e001bb78d20c98c425a88dd2cc38
milushev91 <mmilushev91@gmail.com>
2026-02-07 18:16:56 +0200

    Update meal_planner.py

--- Dictionaries/meal_planner.py
+++ Dictionaries/meal_planner.py
@@ -1,6 +1,32 @@
 from contents import pantry, recipes
 
+def add_item(item: str, quantity: int, dict: dict) -> None:
+
+    """
+    Adds item and quantity to a dict as key-value pair. If item already
+    in it, it will increase its value by the quantity amount.
+    
+    :param item: Key name
+    :type item: str
+    :param quantity: Amount to add
+    :type quantity: int
+    :param dict: The dictionary that will store the pair.
+    :type dict: dict
+
+    :return: None
+    """
+
+    # if item not in dict:
+    #     dict[item] = 0
+    
+    # dict[item] += quantity
+    # the above code can be simplified by using setdefault() method of dict
+    # setdefault() will return the value of the key if it is in the dict, 
+    # otherwise it will set the key to the default value (0 in this case) and return it.
+    dict[item] = dict.setdefault(item, 0) + quantity
+
 display_dict = {}
+items_to_buy = {}
 for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
 
@@ -20,15 +46,20 @@ while True:
         print(f"You have selected {selected_item}")
         print("checking ingredients...")
         ingredients = recipes[selected_item]
+    
  
-        for item, required_quantity in ingredients:
+        for item, required_quantity in ingredients.items():
             if item in pantry:
                 available_quantity = pantry[item]
 
                 if required_quantity > available_quantity:
-                    print(f"You need {required_quantity - available_quantity} more {item}")
+                    needed_quantity = required_quantity - available_quantity
+                    print(f"You need {needed_quantity} more {item}")
+                    add_item(item=item, quantity=needed_quantity, dict=items_to_buy)
                 else:
                     print(f"{item} is OK")
 
             else:
-                print(f"{item} is NOK")
+                print(f"{item} is not found in your pantry. Please buy.")
+                add_item(item=item, quantity=needed_quantity, dict=items_to_buy)
+
