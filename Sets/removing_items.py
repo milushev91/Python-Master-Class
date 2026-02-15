3640a6494d6d1b05dd84d2a28a56961443e0a114
milushev91 <mmilushev91@gmail.com>
2026-02-15 17:09:45 +0200

    Create removing_items.py

---
+++ Sets/removing_items.py
@@ -0,0 +1,10 @@
+small_ints = set(range(21))
+print(small_ints)
+
+# clear() removes all items from a set
+# small_ints.clear()
+# print(small_ints) # set()
+
+# to remove items they are two methods:
+# 1. discard() - removes item if it finds it, if the item is not inside - nothing happens
+# 2. remove() - removes item if inside, if it isn't raises an error
