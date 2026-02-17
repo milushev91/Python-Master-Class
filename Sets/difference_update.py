a074bf1e0f1596067474fdd6e02227100e4e37ff
milushev91 <mmilushev91@gmail.com>
2026-02-17 07:47:47 +0200

    Create difference_update.py

---
+++ Sets/difference_update.py
@@ -0,0 +1,61 @@
+travel_mode = {"1": "car", "2": "plane"}
+
+items = {
+    "can opener",
+    "fuel",
+    "jumper",
+    "knife",
+    "matches",
+    "razor blades",
+    "razor",
+    "scissors",
+    "shampoo",
+    "shaving cream",
+    "shirts (3)",
+    "shorts",
+    "sleeping bag(s)",
+    "soap",
+    "socks (3 pairs)",
+    "stove",
+    "tent",
+    "mug",
+    "toothbrush",
+    "toothpaste",
+    "towel",
+    "underwear (3 pairs)",
+    "water carrier",
+}
+
+restricted_items = {
+    "catapult",
+    "fuel",
+    "gun",
+    "knife",
+    "razor blades",
+    "scissors",
+    "shampoo",
+}
+
+print("Please choose your mode of travel:")
+for key, value in travel_mode.items():
+    print(f"{key}: {value}")
+    # Python 3.5 and earlier
+    # print("{}: {}".format(key, value))
+
+mode = "-"
+while mode not in travel_mode:
+    mode = input("> ")
+
+if mode == "2":
+    # travelling by plane, remove restricted items
+    # for item in restricted_items:
+    #   items.discard(item)
+    #1 first option
+    # items -= restricted_items
+    # update the set by leave only unique items that are not present in the other set
+    items.difference_update(restricted_items)
+
+# print the packing list
+print("You need to pack:")
+for item in sorted(items):
+    print(item)
